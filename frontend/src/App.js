cc07b9dd0c0cafa87f601dc901d8c2640e0bfeb3
IG <iikim2511@gmail.com>
2022-08-04 00:43:37 -0400

    c4

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,28 @@
 
 
+/* 🦄🦄c4. React Setup & Git Initialize, package.json, gitignore 
 
-function App() {
+  🍀git folder
+    <내방식 (rm -rf .git이 에러나서 안됨..)>
+
+    root folder에 git init
+
+    frontend folder 에 npx create-react-app .
+
+    root folder에 이미 git폴더가 있어서, fontend flolder에 git이 설치 안됨
+
+  🍀gitignore file
+    .gitignore파일 --> root 폴더로 옮김
+
+    .gitignore파일에 추가
+    node_modules
+    node_modules/
+    .env  👉 global varialbe  - 비밀키들 포함되어있음
+*/
+
+// 🍀c4
+// function App() {
+const App = () =>{
   return (
     <div className="App">
       asdfasdf
