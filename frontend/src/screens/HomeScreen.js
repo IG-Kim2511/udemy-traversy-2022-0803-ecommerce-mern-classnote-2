20ccc95c28ee72e304f1421dc87d4256761ceddc
IG <iikim2511@gmail.com>
2022-09-01 15:58:59 -0400

    c7-fin

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -17,7 +17,7 @@ const HomeScreen = () => {
         <h1>Latest Products</h1>
         <Row>
             {data_products.map((product)=>(
-                <Col sm={12} md={6} lg={4} xl={3}>
+                <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
                     <h3>{product.name}</h3>
                     <Product product={product}/>
                 </Col>
