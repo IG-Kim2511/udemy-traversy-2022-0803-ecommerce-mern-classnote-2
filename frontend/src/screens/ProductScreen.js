e86ec8b19382973f0ae7ff969644cf3bda7f0ca8
IG <iikim2511@gmail.com>
2022-09-02 14:16:59 -0400

    ing

---
+++ frontend/src/screens/ProductScreen.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const ProductScreen = () => {
+  return (
+    <div>
+    <h3 className="components_name">ProductScreen</h3>
+    
+    </div>
+  )
+}
+
+export default ProductScreen
