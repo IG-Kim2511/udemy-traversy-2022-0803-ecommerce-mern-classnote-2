98c70a7487caba8c903d022d25ec6d4688ca0225
IG <iikim2511@gmail.com>
2022-09-02 23:02:06 -0400

    c10. useParams

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,13 +1,26 @@
 import React from 'react'
+import { useParams } from 'react-router-dom';
 import data_products from '../data_products'
 
-const ProductScreen = ({match}) => {
+const ProductScreen = () => {
 
-  // const product = data_products.find((p)=> p._id === match.params.id);
+  // 🦄c10. useParams
+  /* 
+  🍀c10-10. 
+  useParams 사용방법  (수업에서 한 방식과는 다름)
+  const {id} = useParams(); 사용함  
+  */
+  const { id } = useParams();
+  const product = data_products.find((p)=> p._id === id);
+
+  console.log(product)
+
   return (
     <div>
         <h3 className='components_name'>ProductScreen</h3>
  
+        <div>🍀c10-10. </div>
+        <div>{product.name}</div>
     </div>
   )
 }
