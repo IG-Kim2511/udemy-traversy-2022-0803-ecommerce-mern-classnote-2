f0cb6d3ca6b2f893acf9743627bca8ac546a5a88
IG <iikim2511@gmail.com>
2022-09-09 12:32:49 -0400

    c21-fin

--- backend/data/users.js
+++ backend/data/users.js
@@ -3,35 +3,36 @@
 🦄c21 
 🍀👉1 backend/models/userModel.js 
 userModel...mongoose schema에 이미 있는 object key만 가져옴
-
-
-
-🍀
-
-
-🍀
-삭제
-default value 가 false 이므로..
-
 */
+
+/* 
+var bcrypt = require('bcryptjs');
+var salt = bcrypt.genSaltSync(10);
+var hash = bcrypt.hashSync("B4c0/\/", salt); */
+
+
+
+import bcrypt from 'bcryptjs'
 
 const users =[
     {
         name:'Admin User',
         email:'admin@example.com',
-        password: xxxx,             //🍀다른곳에서 비번 가져와서 넣을 예정
+        password: bcrypt.hashSync("123456", 10),             //🍀다른곳에서 비번 가져와서 넣을 예정
         isAdmin: true,
     },
     {
         name:'Admin User',
         email:'admin@example.com',
-        password: xxxx,            
+        password: bcrypt.hashSync("123456", 10), 
         // isAdmin: true,               //🍀삭제, default value 가 false 이므로..
     },
     {
         name:'Admin User',
         email:'admin@example.com',
-        password: xxxx,             
+        password: bcrypt.hashSync("123456", 10),        
         // isAdmin: true,              //🍀삭제, default value 가 false 이므로..
     },
 ]
+
+export default users
